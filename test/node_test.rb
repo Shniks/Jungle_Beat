62fc4ad5375ad1731607172cb52191b54e705e0f
Nikhil Shahi <shniks@gmail.com>
2022-07-04 18:18:45 -0700

    Created test and methods for the Node class.

---
+++ test/node_test.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+
+  def test_if_it_exists
+    node = Node.new("plop")
+
+    assert_equal "plop", node.data
+    assert_nil node.next_node
+  end
+
+end
